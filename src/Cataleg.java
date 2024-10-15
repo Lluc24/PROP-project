c1766ab7ed59100b329519dcb16c49e89ba2a9f9
Alito18 <alorenzo.ln2004@gmail.com>
2024-10-15 15:51:45 +0200

    Cataleg/Producte v2.0

--- src/Cataleg.java
+++ src/Cataleg.java
@@ -2,125 +2,238 @@
 import Producte;
 import pair;
 import java.util.ArrayList;
-
+/**
+ * Cataleg representa el conjunt de productes (@see Classe Productes) que es troben dins del sistema
+ * @author Alejandro Lorenzo Navarro
+ * @version 2.0
+ * 
+ * Tots el productes estan dins de un Arraylist. 
+ * Es poden identificar tant per nom o per index.
+ * Basat en el funcionament de ArrayList no hi ha espais buits entre productes.
+ * Tots el index gerenats per aquesta classe son valits sempre que no s'elimini nigun element de la llista, 
+ * afegir-ne no causa problemas
+ * 
+ * Alguns metodes tenen dos versions, que es diferencien per el parametres que reben, el nom del producte o el seu index.
+ * Es rellevant saber que els metodes que reben un index tenen un cost menor, ja que no es fan busquedes dels productes
+ * es pren com a condicio que el dos indexos representen productes dins de la classe.
+ * 
+ * El primer index sempre tindra valor '0', el metode num_prod_actual sempre retorna la mida correcte del cataleg, 
+ * es a dir el index seguent al ultim producte.
+ */
 public class Cataleg {
     //Classe Cataleg
      //Atributs
 
-     private int Current_numProd;
+     /** Un Array List Representa el conjunt de productes es troben din del sistema */
      private ArrayList<Producte> Cataleg_Productes;
-     private ArrayList<ArrayList<Int>> Cataleg_Similituds; // Matriu de numProd x NumProds, les similituds de tots els productes amb tots el productes
 
      // Constructor
      /**
-          Descripció: Es crea un objecte cataleg buit, sense cap producte
-      */
+      * Descripció: Es crea un objecte cataleg buit, sense cap producte
+     */
      public Cataleg() {
           //creacio Cataleg Productes
-          ArrayList<Producte> aux1 = new ArrayList<>();
-          aux1 = this.Cataleg_Productes;
-          find_prod(nou)ilituds
-          ArrayList<ArrayList<Int>> aux2 = new ArrayList<>()();
-          aux2 = this.Cataleg_Similituds;
-
-          this.Current_numProd = 0;
-
+          ArrayList<Producte> aux = new ArrayList<Producte>();
+          this.Cataleg_Productes = aux;
      }
 
      //Metodes
      /**
-          Descripció: S'afegeix un producte si aquest no es troba al cataleg, també afegira 
-          Parametres: Rep un produte nou, i un llista de similituds, donada per un producte i la similitud corresponent amb el producte
-          Pre: Els valors del producte son correctes
-     */
-     public void afegir_producte(Producte nou, pair<String, int>[] lista_simi) {
-          
-          //Si es troba el producte a cataleg es llença error
-          if (Cataleg_Productes.contains(nou)) return; //error
-          
-          Cataleg_Productes.add(nou);
+      * Descripció: S'afegeix un producte al final del cataleg si no es troba al cataleg un amb el mateix nom, 
+      * tambe es confiquren les similtuts associades amb el nou producte.
+      * Aquests metode se ha de fer servir per la creacio i de productes dins del sistema.
+      * @param new_nom Nom del producte a afegir
+      * @param llista_simi Similituds dels productes, <Nom_Productes, Similitud>
+      */
+     public void afegir_producte(String new_nom, pair<String, Int>[] llista_simi) {
+          //Existe producto con new_nom
+          if (Cataleg_Productes.find_prod(new_nom)) return; //Error
 
-          //Afegim el index al nou producte
-          int nou_index = Cataleg_Productes.size-1;
-          Cataleg_Productes(nou_index).index = nou_index;
-
-          //Guardem les seves similituds
-          ArrayList<int> new_simi = new ArrayList<>();
-          for (int i = 0; i < lista_simi.lenght; ++i) {
-               Producte aux = find_prod(lista_simi[i]); 
-               if (aux != null) {
-                    int index_aux = Cataleg_Productes.indexof(aux);
-                    Cataleg_Similituds(nou_index).add(index_aux, lista_simi.second);
-               } else {
-                    //error el producte no es troba al cataleg
+          //Nova instancia producta
+          int new_index = Cataleg_Productes.size();
+          ArrayList<Int> new_simi = new ArrayList<Int>(new_index+1);
+          
+               //Afegim les similituds a la llista
+               for (int i = 0; i < llista_simi.size(); ++i) {
+                    int index_in = index_prod(llista_simi(i).first);
+                    new_simi.add(index_in, llista_simi(i).second); //Afegim a la nova llista de similituds
+                    Cataleg_Productes(index_in).llista_Similituds.add(new_index, llista_simi(i).second); //La similitud del producte preexisten amb el nou
                }
-          }
-          Cataleg_Similituds(nou_index).add(nou_index, 0); 
-          ++Current_numProd;
-          
+
+          Producte new_prod = new Producte(new_index, new_nom, new_simi);
+          Cataleg_Productes.add(new_index, new_prod);
 
      }
 
      /**
-          Descripció: Elimina el producte si existeix dins del cataleg
-     */
-     public void eliminar_producte(Producte eliminat) {
+      * Descripció: S'elimina un producte, i totes les similituds associades determinat.
+      * Warning: Tots el indexos inicialitzats localment abans de l'execucio de la funció seran erronis
+      * @param index_out Index del producte a eliminar
+      */
+     public void eliminar_producte_index(int index_out) {
+          Cataleg_Productes.remove(index_out);
+
+          //Eliminem totes les similituds associades al producte
+          for (int i = 0; i < Cataleg_Productes.size(); ++i) {
+               Cataleg_Productes(i).llista_Similituds.remove(index_out); 
+          }
+
+          //Actualitzem tots els index del productes
+          for (int i = index_out; i < Cataleg_Productes.size(); ++i) {
+               Cataleg_Productes(i).index = i;
+          }
+     }
+
+     /**
+      * Descripció: S'elimina un producte, i totes les similituds associades determinat si aquest es troba dins del cataleg
+      * Warning: Tots el indexos inicialitzats localment abans de l'execucio de la funció seran erronis
+      * @param nom_out Nom del producte que es vol eliminar
+      * @see eliminar_producte_index(int index_out)
+      */
+     public void eliminar_producte_nom(String nom_out) {
+          
+          //Busquem l'index del producte a eliminar
+          int index_out = Cataleg_Productes.index_prod(nom_out);
           
           //Si no es troba el producte a cataleg es llença error
-          if (!Cataleg_Productes.contains(nou)) return; //error
-          
-          int index_out = eliminat.index;
-          //Eliminem el producte...
-          Cataleg_Productes.remove(index_out);
-          //...I les seves similituds
-          for (int i = 0; i < Cataleg_Similituds(index_out).size(); ++i) {
-               Cataleg_Similituds(index_out).remove(i);     
-          }
+          if (index_out == -1) return; //error
 
-
-          --Current_numProd;
+          eliminar_producte_index((index_out));
      }
 
      /**
-          Descripció:
-     */
-     public void editar_similitud(Producte prod1, Producte prod2, int new_simi) {
-          if (!Cataleg_Productes.contains(prod1) && !Cataleg_Productes.contains(prod2)) return; //error
-          int index_1 = prod1.index;
-          int index_2 = prod2.index;
+      *  Descripció: Modifica la similituts entre el dos productes donats tal que es igual a la similitud donada
+      * @param index_prod1 Index del primer producte
+      * @param index_prod2 Index del segon producte
+      * @param new_simi Nova similitud
+      */
+     public void editar_similitud_index(int index_prod1, int index_prod2, int new_simi) {
+          //Modifiquem les similituds
+          Cataleg_Productes(index1).llista_Similituds.set(index2, new_simi);
+          Cataleg_Productes(index2).llista_Similituds.set(index1, new_simi);
+     }
 
-          Cataleg_Similituds(index_1)(index_2) = new_simi;
+     /**
+      *  Descripció: Modifica la similituts entre el dos productes donats tal que es igual a la similitud donada
+      *  Si algun dels productes no es troba al cataleg es es dona error
+      * @param nom_prod1 String que representa el nom del primer producte
+      * @param nom_prod2 String que representa el nom del segon producte
+      * @param new_simi Nova similitud entre el dos productes
+      */
+     public void editar_similitud(String nom_prod1, String nom_prod2, int new_simi) {
+          int index1 =Cataleg_Productes.index_prod(nom_prod1);
+          if (index1 == -1) return; //error
+
+
+          int index2 = Cataleg_Productes.index_prod(nom_prod2);
+          if (index2 == -1) return; //error
+
+          //Modifiquem les similituds
+          Cataleg_Productes(index1).llista_Similituds.set(index2, new_simi);
+          Cataleg_Productes(index2).llista_Similituds.set(index1, new_simi);
+
      }
 
      //Consultores
      /**
-          Descripció: Retornem el nombre actual de productes dins del cataleg
+      * 
+      * @param nom Nom del producte a buscar
+      * @return True si es troba dins del cataleg, si no FALSE
       */
-     public void num_Prod_act() {
-          return Current_numProd;
+     public boolean find_prod(String nom_in){
+          for (int i = 0; i < Cataleg_Productes.size(); ++i) {
+               if (Cataleg_Productes(i).nom == nom_in) return true;
+          }
+          return false;
+
+     }
+
+     public boolean valida_index(int index_in) {
+          return (index_in < Cataleg_Productes.size()); 
      }
 
      /**
-          Descripció: Retorna el producte amb nom igual al string donat com a parametre
-     */
-     public producte consultar_producte(String nom) {
+      * 
+      * @param nom Nom del producte a buscar
+      * @return Retorna el index del producte amb el nom =  nom_in, si no es troba el producte retorna -1
+      */
+     public int index_prod(String nom_in){
           for (int i = 0; i < Cataleg_Productes.size(); ++i) {
-               if (Cataleg_Productes(i).nom = nom) return Cataleg_Productes(i);
+               if (Cataleg_Productes(i).nom == nom_in) return i;
           }
-          return null; //Que retornem si no hi ha res
+          return -1;
+     }
 
-     }void
 
      /**
-          Parametres: Dos Productes prod1, prod2
-          Descripció: Donat dos productes es retorna la seva similitud si ambdos productes estan dins de cataleg, si no es aixi es retorna -1
-     */ 
-     public int consultar_similitud(Producte prod1, Producte prod2){
-          if (!Cataleg_Productes.contains(prod1) && !Cataleg_Productes.contains(prod2)) return -1; //error
-          int index_1 = prod1.index;
-          int index_2 = prod2.index;
+      * 
+      * @param nom Nom del producte a buscar
+      * @return Retorna el producte amb nom = nom_in al parametre nom
+      * @see idenx_prod(String nom_in)
+      */
+     public producte consultar_producte(String nom_in) {
+          int index_out = Cataleg_Productes.index_prod(nom_in);
+          if (index_out == -1) return null; //Que retornem si no hi es el producte
+          else return Cataleg_Productes(index_out);
+     }
 
-          return Cataleg_Similituds(index_1)(index_2);
+     /**
+      * Pre: El indexos han de representar productes que es troben al cataleg
+      * @param index_prod1 Index del primer prodcute
+      * @param index_prod2 Index del segon producte
+      * @return La similitud entre el dos productes 
+      */
+     public int consultar_similitud_index(int index_prod1, int index_prod2) {
+          return Cataleg_Productes(index_prod1).llista_Similituds(index_prod2);
+     }
+     /**
+      * Descripcio: Retorna la similituds entre dos productes donats, si algun producte no es troba retorna -1
+      * @param nom_prod1 Nom del primer producte
+      * @param nom_prod2 Nom del segon producte
+      * @return Integer igual al valor de la similitud entre el dos productes
+      * @see consultar_similitud_index(int index_prod1, int index_prod2)
+      */
+     public int consultar_similitud_nom(Producte nom_prod1, Producte nom_prod2){
+          int index_prod1 = Cataleg_Productes.index_prod(nom_prod1);
+          if (index_prod1 == -1) return -1;
+          
+          int index_prod2 = Cataleg_Productes.index_prod(nom_prod2);
+          if (index_prod2 == -1) return -1;
+
+          return Cataleg_Productes.consultar_similitud_index(index_prod1, index_prod2);
+     
+     }
+
+    /**
+     * 
+     * @param index_prod Index del producte 
+     * @return Retornar una ArrayList, que representa el valor integer de les similituds del producte
+     */
+    public ArrayList<Int> consultar_similituds_producte(int index_prod) {
+          return Cataleg_Productes(index_prod).llista_Similituds;
+     }
+
+     /**
+      * 
+      * @param nom_prod Nom del producte 
+      * @return Retornar una ArrayList, que representa el valor integer de les similituds del producte, si no es troba retorna null
+      * @see consultar_similituds_producte(int index_prod)
+      */
+    public ArrayList<Int> consultar_similituds_producte(String nom_prod) { 
+          int index_prod = Cataleg_Productes.index_prod(nom_prod);
+          if (index_prod == -1) return null;
+          
+
+
+          return Cataleg_Productes.consultar_similituds_producte(index_prod);
+     }
+
+     /**
+      * 
+      * @return Integer que es igual al nombre de productes al cataleg
+      */
+     public int num_prod_act() {
+          return Cataleg_Productes.size();;
      }
 
      /**
@@ -129,14 +242,5 @@ public class Cataleg {
     public ArrayList<Producte> consultar_cataleg(){
           return Cataleg_Productes;
 
-    }
-
-    /**
-          Descripció: Retorna el cataleg de similituds;
-     */
-    public ArrayList<ArrayList<int>> consultar_cataleg_similitud(){
-          return Cataleg_Similituds;
-
-    }
-
+     }
 }
--- src/Producte.java
+++ src/Producte.java
@@ -1,5 +1,37 @@
+import java.util.ArrayList;
+
+
+/**
+ * Representa un producte del sistema, amb nom, llista de similituts 
+ * i index que representa la seva posicio al Catalge (@see Cataleg)
+ * @author Alejandro Lorenzo Navarro
+ * @version 2.0
+ */
 public class Producte {
 
     public int index;
-    public string nom; 
+    public String nom; 
+    public ArrayList<Int> llista_Similituds;
+
+    /**
+     * @param inde El index a Cataleg_Productes de la classe Cataleg on es troba el producte 
+     * @param nom El nom de producte a crear
+     * @param llista Una llista amb totes les similituds del productes
+     * Descripció: Es crea una nova instancia de productes
+     */
+    public Producte(int index, String nom, ArrayList<Int> llista) {
+        this.index = index;
+        this.nom = nom;
+        this.llista_Similituds = llista;
+    }
+
+    /**
+     * 
+     * @param index_prod El index d'un producte al cual es vol la similitud amb el producte que crida la funcio
+     * @return Retorna el valor de la simiitud This.Producte = Producte.index=index_prod
+     */
+    public int simil_prod(int index_prod) {
+        return llista_Similituds(index_prod);
+    }
+
 }
