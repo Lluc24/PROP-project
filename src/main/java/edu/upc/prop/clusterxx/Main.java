bb90c00e86015c798150fa61937dbe4b326c14e1
lluc.santamaria <lluc.santamaria@estudiantat.upc.edu>
2024-11-13 19:46:41 +0100

    Afegit les llibreries de testing

--- src/main/java/edu/upc/prop/clusterxx/Main.java
+++ src/main/java/edu/upc/prop/clusterxx/Main.java
@@ -1,11 +1,8 @@
 package edu.upc.prop.clusterxx;
 
-import com.google.gson.Gson;
-
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
-    new Gson();
 
     Main divisioner = new Main();
     System.out.println("Dividing 10 by 2 is " + divisioner.division(10,2));
