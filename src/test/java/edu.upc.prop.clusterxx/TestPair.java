4e2d9f94a3a5cb3e6e3ce58a8061242ee39a4b02
Alito18 <alorenzo.ln2004@gmail.com>
2024-11-15 02:37:51 +0100

    Test Producte,, Cataleg, Pair: PASSATS

--- src/test/java/edu.upc.prop.clusterxx/TestPair.java
+++ src/test/java/edu.upc.prop.clusterxx/TestPair.java
@@ -7,28 +7,33 @@ import edu.upc.prop.clusterxx.Cataleg;
 
 import org.junit.Test;
 
-/*
 public class TestPair {
 
     @Test
     public void TestPair() {
+
+        //Proba instancia unitaria
         String a = "Proba_Pair";
         Double d = 1.0;
         pair<String, Double> unit = new pair<>(a, d);
         assertEquals("Proba first",a, unit.first);
         assertEquals("Proba second",d, unit.second);
 
+
+        //Proba array de pair
+        //Creacio array
         pair<String, Double>[] array = new pair[5];
         for (int i = 0; i < 5; ++i) {
             String prod = "Prod_" + i;
             pair<String, Double> aux = new pair<>(prod, i * 1.0);
             array[i] = aux;
         }
-
+        //Comprovacio Array
         for (int i = 0; i < 5; ++i) {
             assertEquals("Proba vector first","Prod_"+i, array[i].first);
-            assertEquals("Proba vector second",i*1.0, array[i].second);
+            Double p = i*1.0;
+            assertEquals("Proba vector second",p, array[i].second);
         }
     }
 }
-*/
+
