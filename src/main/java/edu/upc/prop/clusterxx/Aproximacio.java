9e664bdcc7431faba3fefb3566c25fd8d0b427d8
lluc.santamaria <lluc.santamaria@estudiantat.upc.edu>
2024-11-16 12:35:47 +0100

    Migrat de ParellInt a Pair

--- src/main/java/edu/upc/prop/clusterxx/Aproximacio.java
+++ src/main/java/edu/upc/prop/clusterxx/Aproximacio.java
@@ -9,82 +9,104 @@ public class Aproximacio extends Algorisme {
     @Override
     public int[] solucionar(double[][] matriuSimilituds) {
         n = matriuSimilituds.length;
-        if (n == 0) return new int[0];
         m = n * (n - 1) / 2;
         similituds = matriuSimilituds;
-        ParellInt[] arestesOrdenades = new ParellInt[m];
+
+        // Cas base:
+        if (n == 0) return new int[0];
+        if (n == 1) return new int[]{0};
+
+        // Cas extens:
+
+        // Construim un array amb totes les arestes del graf
+        Pair<Integer, Integer>[] arestesOrdenades = new Pair[m];
         int k = 0;
         for (int i = 0; i < n; ++i) {
             for (int j = i + 1; j < n; ++j) {
-                arestesOrdenades[k] = new ParellInt(i, j);
+                arestesOrdenades[k] = new Pair<>(i, j);
                 ++k;
             }
         }
 
+        // Ordenem les arestes per pes creixent utilitzant QUICKSORT
         ordenacioRapida(arestesOrdenades, 0, m-1);
+
+        // Obtenim l'arbre d'expansio maxim MST. En total te n-1 arestes
         int[] idxArestesMST = kruskal(arestesOrdenades);
 
+        // Construim el digraf on cada aresta no dirigida passa a ser dos arcs en sentits oposats
+        // Per representar el digraf utilitzem llistes d'adjacencia
         List<Set<Integer>> grafDobleDirigit = new ArrayList<Set<Integer>>(n);
         for (int i = 0; i < n; ++i) grafDobleDirigit.add(new HashSet<Integer>());
-        for (int i = 0; i < n - 1; ++i) {
-            ParellInt aresta = arestesOrdenades[idxArestesMST[i]];
-            grafDobleDirigit.get(aresta.getPrimer()).add(aresta.getSegon());
-            grafDobleDirigit.get(aresta.getSegon()).add(aresta.getPrimer());
+        for (int i = 0; i < n - 1; ++i) { // Recorrem les arestes del MST
+            Pair<Integer, Integer> aresta = arestesOrdenades[idxArestesMST[i]]; // aresta = (u, v)
+            grafDobleDirigit.get(aresta.first).add(aresta.second); // Afegim l'arc u -> v
+            grafDobleDirigit.get(aresta.second).add(aresta.first); // Afegim l'arc v -> u
         }
 
-        List<Integer> cicle = new ArrayList<Integer>();
-        dfs(grafDobleDirigit, cicle, 0, -1);
+        // Trobem un cicle euleria a partir de recorrer el digraf amb un depth-first search
+        List<Integer> cicleEuleria = new ArrayList<Integer>();
+        dfs(grafDobleDirigit, cicleEuleria, 0, -1);
 
-        System.out.print("Cicle euleria: ");
-        for (int i = 0; i < cicle.size(); ++i) System.out.print(cicle.get(i) + " ");
-        System.out.println();
-        simplificar(cicle);
-        System.out.print("Cicle hamiltonia: ");
-        for (int i = 0; i < cicle.size(); ++i) System.out.print(cicle.get(i) + " ");
-        System.out.println();
-        int[] cicleHam = new int[n];
-        for (int i = 0; i < n; ++i) cicleHam[i] = cicle.get(i);
-        return cicleHam;
+        // Simplifiquem el cicle euleria per transformar-lo en cicle hamiltonia
+        List<Integer> cicleHamiltonia = new ArrayList<Integer>(cicleEuleria);
+        simplificar(cicleHamiltonia);
+
+        // Reconstruim el array que sera el cami hamiltonia solucio
+        int[] camiHamiltonia = new int[n];
+        for (int i = 0; i < n; ++i) camiHamiltonia[i] = cicleHamiltonia.get(i);
+        return camiHamiltonia;
     }
 
-    void ordenacioRapida(ParellInt[] arestes, int e, int d) {
+    void ordenacioRapida(Pair<Integer, Integer>[] arestes, int e, int d) {
         if (e < d) {
-            int q = particioHoare(arestes, e, d);
+            int q = particioHoare(arestes, e, d);   // q es la posicio que deixa a la seva esquerra les arestes de menor
+                                                    // pes que l'aresta q i la resta a la dreta
+            // Apliquem recursivament l'ordenacio en l'esquerra i la dreta obtenint arestes[e..d] ordenades per pes
             ordenacioRapida(arestes, e, q);
             ordenacioRapida(arestes, q + 1, d);
         }
     }
 
-    int particioHoare(ParellInt[] adj, int e, int d) {
-        double pivot = similituds[adj[e].getPrimer()][adj[e].getSegon()];
+    int particioHoare(Pair<Integer, Integer>[] arestes, int e, int d) {
+        double pivot = similituds[arestes[e].first][arestes[e].second]; // Utilitzem com a pivot la primera aresta
         int i = e;
         int j = d;
         boolean acabat = false;
         while (!acabat) {
-            while (similituds[adj[i].getPrimer()][adj[i].getSegon()] < pivot) ++i;
-            while (similituds[adj[j].getPrimer()][adj[j].getSegon()] > pivot) --j;
+            // Obtenim les posicions i, j tals que:
+            // Tota aresta a l'equerra d'i te menor pes que l'aresta de la posicio pivot
+            // Tota aresta a la dreta de j te major pes que l'aresta de la posicio pivot
+            while (similituds[arestes[i].first][arestes[i].second] < pivot) ++i;
+            while (similituds[arestes[j].first][arestes[j].second] > pivot) --j;
             if (i >= j) acabat = true;
             else {
-                ParellInt aux = adj[i];
-                adj[i] = adj[j];
-                adj[j] = aux;
+                // Intercambiem les posicions i, j
+                Pair<Integer, Integer> aux = arestes[i];
+                arestes[i] = arestes[j];
+                arestes[j] = aux;
                 ++i;
                 --j;
+                // Es compleix que:
+                // arestes[e..i-1] tenen pes menor o igual a l'aresta pivot
+                // arestes[j+1..d] tenen pes major o igual a l'aresta pivot
             }
         }
+        // Retornem la posicio on hi ha una aresta amb el mateix pes que l'aresta pivot
         return j;
     }
 
-    int[] kruskal(ParellInt[] arestesOrdenades) {
+    int[] kruskal(Pair<Integer, Integer>[] arestesOrdenades) {
         MergeFindSet mfs = new MergeFindSet(n);
-        int[] idxArestes = new int[n-1];
-        idxArestes[0] = m-1;
+        int[] idxArestes = new int[n-1]; // Hem de trobar els indexos de les n - 1 arestes que construeixen el MST
+        idxArestes[0] = m-1; // La primera aresta sera la de major pes, amb una aresta no fem cap cicle
         int arestesFixades = 1;
         int i = m-2;
-        mfs.unir(arestesOrdenades[m-1].getPrimer(), arestesOrdenades[m-1].getSegon());
+        mfs.unir(arestesOrdenades[m-1].first, arestesOrdenades[m-1].second);
 
-        while (mfs.getNombreConjunts() > 1) {
-            if (mfs.unir(arestesOrdenades[i].getPrimer(), arestesOrdenades[i].getSegon())) {
+        while (mfs.getNombreConjunts() > 1) { // Mentre no sigui connex
+            if (mfs.unir(arestesOrdenades[i].first, arestesOrdenades[i].second)) {
+                // Hem trobat l'aresta i de maxim pes que no fa un cicle
                 idxArestes[arestesFixades] = i;
                 ++arestesFixades;
             }
@@ -94,11 +116,12 @@ public class Aproximacio extends Algorisme {
     }
 
     void dfs(List<Set<Integer>> grafDobleDirigit, List<Integer> cicle, int vtxActual, int vtxPrevi) {
-        cicle.add(vtxActual);
+        // Cada vertex que es visita s'afegeix al cicle, sigui per expansio de branques o per backtracking
+        cicle.add(vtxActual); // Vertex afegir per expansio
         for (int proper : grafDobleDirigit.get(vtxActual)) {
             if (proper != vtxPrevi) {
                 dfs(grafDobleDirigit, cicle, proper, vtxActual);
-                cicle.add(vtxActual);
+                cicle.add(vtxActual); // Vertex afegit per backtracking
             }
         }
     }
@@ -106,55 +129,78 @@ public class Aproximacio extends Algorisme {
     void simplificar(List<Integer> cicle) {
         int tallaCicle = cicle.size();
         while (tallaCicle > n + 1) {
-            int[] frequencia = new int[n];
+            int[] frequencia = new int[n]; // frequencia indica quantes vegades apareix un vertex en el cicle
             for (int i = 1; i < n; ++i) frequencia[i] = 0;
             frequencia[0] = 1;
-            ArrayList<ParellInt> idxsRepeticio = new ArrayList<ParellInt>();
-            ArrayList<Double> costRepeticio = new ArrayList<Double>();
+
+            List<Pair<Pair<Integer, Integer>, Double>> repeticions = new ArrayList<Pair<Pair<Integer, Integer>, Double>>();
+            // Una llista de la forma {((u, v), w), ...}
+            // Cada element es una repeticio (u, v), es a dir, els vertexs del cicle en les posicions u, v salten
+            // els vertexs repetits i w es el pes que correspon a la diferencia de pes en cas d'eliminar la repeticio
             int idxRepeticio = -1;
             int vtxPrevi = 0;
+
+            // El vertex inicial (posicio 0) i final (posicio tallaCicle-1) no pertanyen a repeticions per definicio
             for (int i = 1; i < tallaCicle-1; ++i) {
                 int vtxActual = cicle.get(i);
-                if (frequencia[vtxActual] > 0) {
-                    if (frequencia[vtxPrevi] > 1) {
-                        double costAcumulat = costRepeticio.get(idxRepeticio);
-                        costRepeticio.set(idxRepeticio, costAcumulat + similituds[vtxPrevi][vtxActual]);
+                // Per poder estudiar les repeticions, considerarem tres casos principals:
+                if (frequencia[vtxActual] > 0) {    // Cas 1: El vertex actual ja ha sigut visitat previament.
+                                                    // Es a dir, pertany a una repeticio
+                    // Distingin dos subcasos:
+                    if (frequencia[vtxPrevi] > 1) { // Subcas 1: El vertex previ tambe havia sigut visitat previament,
+                                                    // tenim una repeticio oberta que li hem d'afegir cost extra
+                                                    // corresponent a l'aresta (vtxPrevi, vtxActual)
+                        repeticions.get(idxRepeticio).second += similituds[vtxPrevi][vtxActual];
                     }
-                    else {
-                        costRepeticio.add(similituds[vtxPrevi][vtxActual]);
-                        idxsRepeticio.add(new ParellInt(i-1, -1));
+                    else {  // Subcas 2: El vertex previ s'havia visitat per primera vegada, hem d'iniciar una nova
+                            // repeticio de vertexs
+                        repeticions.add(new Pair(new Pair(i-1, -1), similituds[vtxPrevi][vtxActual]));
                         ++idxRepeticio;
                     }
                 }
-                else if (frequencia[vtxPrevi] > 1){
-                    double costAcumulat = costRepeticio.get(idxRepeticio);
-                    costRepeticio.set(idxRepeticio, costAcumulat + similituds[vtxPrevi][vtxActual]);
-                    idxsRepeticio.get(idxRepeticio).setSegon(i);
+                else if (frequencia[vtxPrevi] > 1) {    // Cas 2: El vertex actual es visitat per primer cop i el vertex
+                                                        // previ pertany a una repeticio (havia sigut visitat previament).
+                                                        // Hem d'afegir el pes de (vtxPrevi, vtxActual), treure el pes
+                                                        // de (vertex que inicia la repeticio, vtxActual) i establir
+                                                        // vtxActual com a fi de la repeticio. Aixi haurem tancat la
+                                                        // repeticio
+                    Pair<Pair<Integer, Integer>, Double> p = repeticions.get(idxRepeticio);
+                    p.first.second = i;
+                    p.second += similituds[vtxPrevi][vtxActual];
+                    p.second -= similituds[cicle.get(p.first.first)][vtxActual];
+                }
+                else {  // Cas 3: El vertex acual i el previ son visitats per primer cop. No cal fer res
+
                 }
                 ++frequencia[vtxActual];
                 vtxPrevi = vtxActual;
             }
-            if (frequencia[vtxPrevi] > 1) {
-                double costAcumulat = costRepeticio.get(idxRepeticio);
-                costRepeticio.set(idxRepeticio, costAcumulat + similituds[vtxPrevi][cicle.get(tallaCicle-1)]);
-                idxsRepeticio.get(idxRepeticio).setSegon(tallaCicle-1);
+            if (frequencia[vtxPrevi] > 1) { // Estem en el vertex que tanca el cicle, pot ser que el vtxPrevi pertany
+                                            // a una repeticio que no ha sigut tancada. Cal reconsiderar el Cas 2
+                Pair<Pair<Integer, Integer>, Double> p = repeticions.get(idxRepeticio);
+                p.first.second = tallaCicle-1;
+                int vtxActual = cicle.get(tallaCicle-1);
+                p.second += similituds[vtxPrevi][vtxActual];
+                p.second -= similituds[cicle.get(p.first.first)][vtxActual];
             }
+
+            // Tenim totes les repeticions del cicle actual, cal trobar la que tingui una menor diferencia per tal que
+            // el cost total es mantingui el mes gran possible
             int millorCandidat = 0;
-            ParellInt repeCicle = idxsRepeticio.getFirst();
-            ParellInt repeVertexs = new ParellInt(cicle.get(repeCicle.getPrimer()), cicle.get(repeCicle.getSegon()));
-            double menorDiferencia = costRepeticio.getFirst() - similituds[repeVertexs.getPrimer()][repeVertexs.getSegon()];
+            double menorDiferencia = repeticions.get(millorCandidat).second;
             for (int i = 1; i <= idxRepeticio; ++i) {
-                repeCicle = idxsRepeticio.get(i);
-                repeVertexs = new ParellInt(cicle.get(repeCicle.getPrimer()), cicle.get(repeCicle.getSegon()));
-                double diferencia = costRepeticio.get(i) - similituds[repeVertexs.getPrimer()][repeVertexs.getSegon()];
-                if (diferencia < menorDiferencia) {
+                double diferencia = repeticions.get(i).second;
+                if (diferencia < menorDiferencia) { // Hem trobat una repeticio amb menor cost
                     millorCandidat = i;
                     menorDiferencia = diferencia;
                 }
             }
-            int idxInicial = idxsRepeticio.get(millorCandidat).getPrimer() + 1;
-            int idxFinal = idxsRepeticio.get(millorCandidat).getSegon() - 1;
-            tallaCicle -= idxFinal - idxInicial + 1;
+
+            // Tenim la millor repeticio ((u, v), w) a eliminar. Cal eliminar els vertexs que estiguin entre u i v
+            Pair<Integer, Integer> millorParell = repeticions.get(millorCandidat).first;
+            int idxInicial = millorParell.first + 1;
+            int idxFinal = millorParell.second - 1;
+            tallaCicle -= idxFinal - idxInicial + 1;    // Nova talla del cicle un cop eliminants els vertexs entre u i v
             for (int i = idxInicial; i <= idxFinal; ++i) {
                 cicle.remove(idxInicial);
             }
