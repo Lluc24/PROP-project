edd5eb26a6e0a6833b3e4c29e6dedf313ebe8632
Alito18 <alorenzo.ln2004@gmail.com>
2024-11-18 19:13:17 +0100

    Error espai

--- src/main/java/layers/domain/controllers/CtrlCatalegAmbRestriccions.java
+++ src/main/java/layers/domain/controllers/CtrlCatalegAmbRestriccions.java
@@ -263,7 +263,7 @@ public class CtrlCatalegAmbRestriccions extends CtrlCataleg {
                     Pair<Integer, Double> p = new Pair<>(i, llista_simi[i].second);
                     simi_index[i] = p;
                 } else {
-                    throw new FormatInputNoValid("El valors de la similitud per producte" +llista_simi[i].first+"no son valid");
+                    throw new FormatInputNoValid("El valors de la similitud per producte " +llista_simi[i].first+" no son valid");
                 }
             }
 
