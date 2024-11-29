41c9b038d7df9d46da13c1a500348812cad3c820
lluc.santamaria <lluc.santamaria@estudiantat.upc.edu>
2024-11-29 18:51:57 +0100

    Implementada VistaPrincipal

--- src/main/java/layers/presentation/views/VistaPrincipal.java
+++ src/main/java/layers/presentation/views/VistaPrincipal.java
@@ -9,9 +9,9 @@ public class VistaPrincipal extends VistaGeneric {
     private CtrlVistaCatalegAmbRestriccions ctrlVistaCatalegAmbRestriccions;
     private CtrlVistaSolucions ctrlVistaSolucions;
 
-    private JFrame frameVista = new JFrame("Vista Principal");
+    private JFrame frameVista;
     private JPanel panelContingut;
-    private JButton botoCataleg = new JButton("Gestionar  cataleg");
+    private JButton botoCataleg;
     private JButton botoSolucions = new JButton("Gestionar Solucions");
 
     public void executar(CtrlVistaGeneric ctrl1, CtrlVistaGeneric ctrl2) {
@@ -22,10 +22,41 @@ public class VistaPrincipal extends VistaGeneric {
     }
 
     private void inicialitzarComponents() {
+
+        // Inicialitzem el frame
+        frameVista = new JFrame("Vista Principal");
         frameVista.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // Inicialitzem el panel
         panelContingut = (JPanel) frameVista.getContentPane();
-        panelContingut.add(botoCataleg);
-        panelContingut.add(botoSolucions);
-        frameVista.add(panelContingut);
+        panel.setLayout(new FlowLayout());
+
+        // Inicialitzem el boto Gestionar Cataleg
+        botoCataleg = new JButton("Gestionar Cataleg");
+        botoCataleg.setSize(100, 20);
+        botoCataleg.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                System.out.println("S'ha clicat el boto 'Gestionar Cataleg'");
+                actionPerformedBotoCataleg(event);
+            }
+        });
+
+        // Inicialitzem el boto Gestionar Solucions
+        botoCataleg = new JButton("Gestionar Solucions");
+        botoCataleg.setSize(100, 20);
+        botoCataleg.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                System.out.println("S'ha clicat el boto 'Gestionar Solucions'");
+                actionPerformedBotoSolucions(event);
+            }
+        });
+    }
+
+    private void actionPerformedBotoCataleg(ActionEvent event) {
+        ctrlVistaCatalegAmbRestriccions.executar();
+    }
+
+    private void actionPerformedBotoSolucions(ActionEvent event) {
+        ctrlVistaSolucions.executar();
     }
 }
