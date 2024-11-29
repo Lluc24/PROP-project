1000f9d946cd9797faadad0be1a6b1ec9502616d
lluc.santamaria <lluc.santamaria@estudiantat.upc.edu>
2024-11-29 14:57:17 +0100

    Afegir Vista Generic

---
+++ src/main/java/layers/presentation/views/VistaGeneric.java
@@ -0,0 +1,12 @@
+package layers.presentation.views;
+
+import layers.presentation.controllers.CtrlVistaGeneric;
+
+public class VistaGeneric {
+    public void executar(CtrlVistaGeneric ctrlVista) {
+        System.out.println("Metode executar de VistaGeneric");
+    }
+    public void executar(CtrlVistaGeneric ctrlVista1, CtrlVistaGeneric ctrlVista2) {
+        System.out.println("Metode executar de VistaGeneric");
+    }
+}
