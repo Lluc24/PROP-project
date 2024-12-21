8dca22515ad5298178122f4265e6e67e8d5c6fdd
lluc.santamaria <lluc.santamaria@estudiantat.upc.edu>
2024-12-21 09:42:07 +0100

    Fix inlining main

--- src/main/java/layers/presentation/Main.java
+++ src/main/java/layers/presentation/Main.java
@@ -6,17 +6,16 @@ import layers.presentation.controllers.CtrlVistaGeneric;
 public class Main {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater (
-                new Runnable() {
-                    public void run() {
-                        CtrlPresentacio ctrlPresentacio = CtrlPresentacio.getCtrlPresentacio();
-                        CtrlVistaGeneric ctrlVistaSolucions = ctrlPresentacio.getCtrlVistaSolucions();
-                        CtrlVistaGeneric ctrlVistaCatalegAmbRestriccions = ctrlPresentacio.getCtrlVistaCatalegAmbRestriccions();
+            new Runnable() {
+                public void run() {
+                    CtrlPresentacio ctrlPresentacio = CtrlPresentacio.getCtrlPresentacio();
+                    CtrlVistaGeneric ctrlVistaSolucions = ctrlPresentacio.getCtrlVistaSolucions();
+                    CtrlVistaGeneric ctrlVistaCatalegAmbRestriccions = ctrlPresentacio.getCtrlVistaCatalegAmbRestriccions();
 
-                        VistaPrincipal vistaPrincipal = new VistaPrincipal();
-                        vistaPrincipal.executar(ctrlVistaCatalegAmbRestriccions, ctrlVistaSolucions);
-                    }
+                    VistaPrincipal vistaPrincipal = new VistaPrincipal();
+                    vistaPrincipal.executar(ctrlVistaCatalegAmbRestriccions, ctrlVistaSolucions);
                 }
-
+            }
         );
     }
 }
