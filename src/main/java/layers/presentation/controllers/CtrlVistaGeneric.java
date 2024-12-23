61f4f847327f0f7f2adebdd04370c181f90c9f44
lluc.santamaria <lluc.santamaria@estudiantat.upc.edu>
2024-12-23 17:49:11 +0100

    Javadoc ctrlVistaGeneric

--- src/main/java/layers/presentation/controllers/CtrlVistaGeneric.java
+++ src/main/java/layers/presentation/controllers/CtrlVistaGeneric.java
@@ -3,24 +3,65 @@ package layers.presentation.controllers;
 import layers.domain.controllers.CtrlGeneric;
 import layers.domain.excepcions.FormatInputNoValid;
 
+import javax.swing.*;
+
+/**
+ * @Class CtrlVistaGeneric
+ * @Description Es la superclasse dels controladors de la capa de presentacio.
+ * @author Lluc Santamaria Riba
+ * @version 3.1
+ *
+ * @Information
+ * Tots els controladors de la capa de presentacio hereden d'aquest.
+ * Permet definir comportament i funcions comunes.
+ * Redueix l'acoblament del projecte.
+ */
 public abstract class CtrlVistaGeneric {
+
+    /** Controlador del domini amb el qual treballa. */
     protected CtrlGeneric ctrl = null;
 
+    /**
+     * Constructora de la classe.
+     * @param ctrlGeneric: Controlador de la capa de domini amb el qual treballara.
+     */
     protected CtrlVistaGeneric(CtrlGeneric ctrlGeneric) {
         this.ctrl = ctrlGeneric;
     }
 
+    /**
+     * Constructora per defecte.
+     */
     protected CtrlVistaGeneric() {
     }
 
+    /**
+     * Metode executar que inicia els casos d'us que gestionen els controladors
+     * que hereten d'aquesta classe.
+     */
     public void executar() {
         System.out.println("Metode executar de CtrlVistaGeneric");
     }
 
+    /**
+     * Metode abstracte per importar un fitxer de disc.
+     * @param path: La ruta fins al directori que conte el fitxer.
+     * @param nomFitxer: El nom del fitxer
+     * @throws FormatInputNoValid: Excepcio llencada en cas de que el fitxer no sigui valid
+     */
     public abstract void importar(String path, String nomFitxer) throws FormatInputNoValid;
 
+    /**
+     * Metode abstracte per exportar un fitxer de disc.
+     * @param path: La ruta fins al directori on es guardara el fitxer.
+     * @param nomFitxer: El nom del fitxer
+     * @throws FormatInputNoValid: Excepcio llencada en cas de que el directori o nomFitxer no permeting guardar-lo
+     */
     public abstract void exportar(String path, String nomFitxer) throws FormatInputNoValid;
 
+    /**
+     * Metode executat per acabar l'execucio del sistema.
+     */
     public void sortirSistema() {
         System.exit(0);
     }
